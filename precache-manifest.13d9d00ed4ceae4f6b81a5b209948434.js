f6362afa27da0a813d48a017721a3ab1b3e3bd6e
xuixo <xavings@penguin>
2019-12-16 15:27:44 +0100

    Updates

---
+++ precache-manifest.13d9d00ed4ceae4f6b81a5b209948434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5771ae255b154401b56856f6f64c88f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ba5f1424d75e4b56884",
+    "url": "/static/css/main.4cfe58e7.chunk.css"
+  },
+  {
+    "revision": "de819e25e176c018f0ed",
+    "url": "/static/js/2.c2060fca.chunk.js"
+  },
+  {
+    "revision": "2ba5f1424d75e4b56884",
+    "url": "/static/js/main.124d0129.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
